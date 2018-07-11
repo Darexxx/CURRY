3775bdd7fadb93f3af646ea2274a759098e509f6
Darexxx <41051996+Darexxx@users.noreply.github.com>
2018-07-11 13:29:20 +0700

    Add files via upload

---
+++ curyy.sh
@@ -0,0 +1,87 @@
+#!/system/xbin/Bash
+#Coded By./D@r3xXx
+#CobaCobaToolBikinTool
+clear
+cd module
+echo ""
+echo "Spammer Tools"
+echo "Coded ./D@r3XxX"
+echo ""
+echo "Pilih Tools Gblk"
+echo $green "1.>Spam SMS Tokopedia"
+echo $blue "2.>Spam SMS Telkomnyet"
+echo $green "3.>Spam SMS mataharimall"
+echo $blue "4.>Spam SMS KFC"
+echo $grenn "5.>Spam SMS PHD"
+echo $blue "6.>Spam SMS Whiskas"
+echo $grenn "7.>Spam SMS ZIPAY"
+echo $blue "8.>Spam SMS HooqTV"
+echo $yellow "0.>Keluar"
+read -p "r00t@Fr03ty ~/" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "CURRY" | lolcat
+php tp.php
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "CURRY"
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "CURRY" | lolcat
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "CURRY"
+php kfc.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "CURRY"
+php phd.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet "CURRY"
+php whiskas.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "CURRY"
+php zipay.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+toilet "CURRY"
+php hooq.php
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "Terima Kasih Telah Menggunakan Tools Ini"
+sleep 1
+echo "Thanks To Everyone"
+sleep 1
+echo "bye Fucking People"
+sleep 1
+exit
+fi
